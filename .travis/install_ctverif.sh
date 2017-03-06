000ee47be71a9cbbbb02879492829a032cfefd00
Daniel Schwartz-Narbonne <dsn@amazon.com>
2017-03-06 13:51:50 -0500

    travis first try

---
+++ .travis/install_ctverif.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PROJECT_NAME="verifying-constant-time"
+# clone ct-verif
+#git clone --depth 1 https://github.com/Qthan/verifying-constant-time.git
+
+pwd
+#./z3/z3 -h
+#cd $PROJECT_NAME
+pwd
+git submodule init tools/smack tools/bam-bam-boogieman
+git submodule init tools/boogie tools/z3
+git submodule update
+cd bin
+./provision.sh
+#cd ../..
+#pwd
+#ls
+#make ctequals
