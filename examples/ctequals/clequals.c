0d14baeb5220256d93e4360991bda0af4c2dd672
Daniel Schwartz-Narbonne <dsn@amazon.com>
2017-05-25 17:11:06 -0400

    better comments on the test

--- examples/ctequals/clequals.c
+++ examples/ctequals/clequals.c
@@ -2,7 +2,8 @@
 #include <smack.h>
 #include "ct-verif.h"
 
-//This ought to be code balanced
+//Code Balanced: yes
+//Current status: fails, most likely because of the loop
 int foo(int a, int k) {
   
   public_in(__SMACK_value(a));
@@ -25,8 +26,22 @@ int foo(int a, int k) {
   return a;
 }
 
-
+//Code Balanced: yes
+//Current status: succeeds
+// (from https://github.com/awslabs/s2n/blob/master/tls/s2n_cbc.c#L91 )
 int foo1(uint8_t pub, int k, int i) {
+  //public_in(__SMACK_value(pub));
+  public_in(__SMACK_value(i));
+
+  // From
+  uint8_t mask = ~(0xff << ((i >= k) * 8));
+  return pub & mask;
+}
+
+//Code Balanced: yes
+//Current status: Fails because boogieman can't do public in for i8
+// (from https://github.com/awslabs/s2n/blob/master/tls/s2n_cbc.c#L91 )
+int foo1_1(uint8_t pub, int k, int i) {
   public_in(__SMACK_value(pub));
   public_in(__SMACK_value(i));
 
@@ -35,13 +50,16 @@ int foo1(uint8_t pub, int k, int i) {
   return pub & mask;
 }
 
+
+//Code Balanced: yes
+//Current status: fails, likely because of the loop
 //based on https://github.com/awslabs/s2n/blob/master/tls/s2n_cbc.c#L91
 //padding_length is private
 int foo2(int padding_length, int size, int* data){
 
   public_in(__SMACK_value(size));
-  
-  int check = 8;
+  public_in(__SMACK_value(data));
+  int check = 8;//use a constant for now
   
   int cutoff = check - padding_length;
   int mismatches = 0;
@@ -55,7 +73,8 @@ int foo2(int padding_length, int size, int* data){
   return mismatches;
 }
 
-//code balancing that doesn't work
+//Code Balanced: yes  as long as * and + are the same cost
+//Current status: succeeds
 float foo3(float b, int k)
 {
   public_in(__SMACK_value(b));
@@ -74,7 +93,8 @@ float foo3(float b, int k)
   return a;
 }
 
-//code balancing that works - cause floating point ops are same cost
+//Code Balanced: yes  as long as * and + are the same cost
+//Current status: succeeds
 float foo4(float b, int k)
 {
   public_in(__SMACK_value(b));
@@ -86,14 +106,15 @@ float foo4(float b, int k)
     a = b + b;
 
   if (!k)
-    a = b + b ;
+    a = b * b ;
   else
-    a = b * b;
+    a = b + b;
 
   return a;
 }
 
-//code balancing that actually works
+//Code Balanced: yes  as long as * and + are the same cost
+//Current status: succeeds
 float foo5(float b, int k)
 {
   public_in(__SMACK_value(b));
@@ -112,6 +133,28 @@ float foo5(float b, int k)
   return a;
 }
 
+//Code Balanced: yes
+//Current status: succeeds
+float foo5_1(float b, int k)
+{
+  public_in(__SMACK_value(b));
+  float a;
+  
+  if (k)
+    a = b + b;
+  else
+    a = b;
+
+  if (!k)
+    a +=  b + b ;
+  else
+    a += b;
+
+  return a;
+}
+
+//Code Balanced: no - !k leads to two assignments, k leads to two mults
+//Current status: detects error
 float foo6(float b, int k)
 {
   public_in(__SMACK_value(b));
@@ -129,6 +172,8 @@ float foo6(float b, int k)
   return a;
 }
 
+//Code Balanced: seemingly yes. But actually, the compiler is too smart for us
+//Current status: detects error
 float foo6_1(float b, int k)
 {
   public_in(__SMACK_value(b));
@@ -150,11 +195,16 @@ int add_wrapper(int a, int b) {
   return a + b;
 }
 
+
+//Code Balanced: yes
+//Current status: fails because of how functions are implemented
 int foo7(int a, int k) {
   public_in(__SMACK_value(a));
   return add_wrapper(a,2);
 }
 
+//Code Balanced: yes
+//Current status: success
 int foo8(int a, int k) {
   public_in(__SMACK_value(a));
   int r;
@@ -166,6 +216,8 @@ int foo8(int a, int k) {
   return r;
 }
 
+//Code Balanced: yes
+//Current status: fails because of how functions work
 int foo9(int a, int k) {
   public_in(__SMACK_value(a));
   int r;
@@ -177,8 +229,11 @@ int foo9(int a, int k) {
   return r;
 }
 
+//Code Balanced: yes
+//Current status: fails because of how ghost code works
 int foo10(int a, int k) {
   public_in(__SMACK_value(a));
+  
   __VERIFIER_assume(k == 7 || k == 8);
   return a;
 }
